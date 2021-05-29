cb3314f57e242eb12ac07356bac40cbd8c6c3cc1
MarcoHazan <85013732+MarcoHazan@users.noreply.github.com>
2021-05-29 11:51:53 +0200

    Update LightSeeker.ino

--- code/LightSeeker.ino
+++ code/LightSeeker.ino
@@ -2,39 +2,48 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
+// dichiarazione di variabili
 
 const char* ssid = "Vodafone-84068020";
 const char* password = "ic3dmcfkydtccbi";
 const char* mqtt_server = "test.mosquitto.org";
 
-void readFoto();
-void choosedirect();
- void updateGrow(); 
-void publisheffect();
- 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-int counter;
+int EnginePin1 = D1;
+int EnginePin2 = D2;
+int EnginePin3 = D3;
+int EnginePin4 = D4;
+int PhotoPin1 = D5;
+int PhotoPin2 = D6;
+int velocita = 600;
+int turnTimer = 450;
+boolean turning = false;
 int crescitaf1;
 int crescitaf2;
 int f1storico;
 int f2storico;
 int f1;
 int f2;
-boolean tar;
 Scheduler runner;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-int tarvalues[8];
+// interfaccia funzioni per task
+void choosedirect();
+ void updateGrow(); 
+void publisheffect();
+void stabilizza();
 
-//Task letturafoto(450*TASK_MILLISECOND, TASK_FOREVER, readFoto);
 
+// TASK
 Task updateCrescita(100 * TASK_MILLISECOND, TASK_FOREVER,updateGrow);
 
 Task chooseDirection(600 * TASK_MILLISECOND, TASK_FOREVER, choosedirect);
 
 Task publishEffect(3000 * TASK_MILLISECOND, TASK_FOREVER, publisheffect);
+
+Task stabilizza(turnTimer * TASK_MILLISECOND, TASK_FOREVER, stabilizza);
+
+
 
 void setup_wifi() {
 
@@ -51,43 +60,6 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
   }*/
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  stop();
-}
-
-void stop(){
-  analogWrite(D0,0);
-  analogWrite(D1,0);
-  analogWrite(D2,0);
-  analogWrite(D3,0);
-}
-
-void publisheffect(){
-  String s = String(crescitaf1);
-  char fs[10];
-  s.toCharArray(fs,10);
-  client.publish("ruck/machine",fs);
-  startRun();
-  
-}
-
-void updateGrow(){
-  readFoto();
-  crescitaf1 = crescitaf1 + (f1 - f1storico);
-  crescitaf2 =  crescitaf2 + (f2 - f2storico);
-}
-
-void choosedirect(){
-  if(crescitaf1 < -20 && crescitaf2 < -20){
-    stop();
-    tar = false;
-  }else if(crescitaf1 > crescitaf2 + 10){
-    turnRight();
-  }else if(crescitaf2 > crescitaf1 + 10){
-    turnLeft();
-  }
 }
 
 void reconnect() {
@@ -112,6 +84,103 @@ void reconnect() {
   }
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  //
+}
+
+void publisheffect(){
+  String s = String(crescitaf1);
+  char fs[10];
+  s.toCharArray(fs,10);
+  client.publish("ruck/machine",fs);
+  startRun();
+  
+}
+
+void updateGrow(){
+  readFoto1();
+  readFoto2();
+  crescitaf1 = crescitaf1 + (f1 - f1storico);
+  crescitaf2 =  crescitaf2 + (f2 - f2storico);
+}
+
+void stabilizza(){
+  if(turning && millis() - turnTime > turnTimer){
+    analogWrite(EnginePin1,velocita);
+    analogWrite(EnginePin2,0);
+    analogWrite(EnginePin3,0);
+    analogWrite(EnginePin4,velocita);
+    turning = false;
+  }
+}
+
+void choosedirect(){
+  if(crescitaf1 < -20 && crescitaf2 < -20){
+    stop();
+    tar = false;
+  }else if(crescitaf1 > crescitaf2 + 10){
+    turnRight();
+  }else if(crescitaf2 > crescitaf1 + 10){
+    turnLeft();
+  }
+}
+
+void startRun(){
+  analogWrite(EnginePin1,velocita);
+  analogWrite(EnginePin2,0);
+  analogWrite(EnginePin3,0);
+  analogWrite(EnginePin4,velocita);
+}
+
+
+
+void stop(){
+  analogWrite(EnginePin1,0);
+  analogWrite(EnginePin2,0);
+  analogWrite(EnginePin3,0);
+  analogWrite(EnginePin4,0);
+}
+
+
+
+void turnRight(){
+  analogWrite(EnginePin1,0);
+  analogWrite(EnginePin2,velocita);
+  turnTime = millis();
+  turning = true;
+}
+
+
+
+void turnLeft(){
+  analogWrite(EnginePin3,velocita);
+  analogWrite(EnginePin4,0);
+  turnTime = millis();
+  turning = true;
+}
+
+
+
+void readFoto1(){
+  f1storico = f1;
+  pinMode(PhotoPin1,INPUT);
+  //pinMode(D2,INPUT);
+  pinMode(PhotoPin2,OUTPUT);
+  digitalWrite(PhotoPin2,HIGH);
+  f1 = analogRead(A0) - 40;
+}
+
+
+
+void readFoto2(){
+  f2storico = f2;
+  pinMode(PhotoPin2,INPUT);
+  //pinMode(D2,INPUT);
+  pinMode(PhotoPin1,OUTPUT);
+  digitalWrite(PhotoPin1,HIGH);
+  f2 = analogRead(A0) + 40;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -119,133 +188,22 @@ void setup() {
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   runner.init();
-  //runner.addTask(letturafoto);
-  //letturafoto.enable();
   runner.addTask(chooseDirection);
   chooseDirection.enable();
   runner.addTask(updateCrescita);
   updateCrescita.enable();
   runner.addTask(publishEffect);
   publishEffect.enable();
-  counter = 0;
-  tar = false;
-  crescitaf1 = 0;
-  crescitaf2 = 0;
   readFoto1();
   readFoto2();
-  pinMode(D0,OUTPUT);
-  pinMode(D1,OUTPUT);
-  pinMode(D2,OUTPUT);
-  pinMode(D3,OUTPUT);
-  analogWrite(D0,700);
-  analogWrite(D1,0);
-  analogWrite(D2,0);
-  analogWrite(D3,700);
-}
-
-void turnRight(){
-  analogWrite(D0,0);
-  analogWrite(D1,600);
-  delay(450);
-  analogWrite(D0,600);
-  analogWrite(D1,0);
-}
-
-void turnLeft(){
-  analogWrite(D2,600);
-  analogWrite(D3,0);
-  delay(450);
-  analogWrite(D2,0);
-  analogWrite(D3,600);
-}
-
-
-void gira(int ngiri){
-  if(ngiri > 4){
-    int act = ngiri - 4;
-    analogWrite(D0,0);
-    analogWrite(D1,600);
-    analogWrite(D2,0);
-    analogWrite(D3,600);
-    for(int i=0;i< act;i++){
-      delay(450);
-    }
-  }else{
-    analogWrite(D0,600);
-    analogWrite(D1,0);
-    analogWrite(D2,600);
-    analogWrite(D3,0);
-    for(int i=0;i<ngiri;i++){
-      delay(450);
-    }
-    startRun();
-  }
-}
-
-void readFoto1(){
-  f1storico = f1;
-  pinMode(D5,INPUT);
-  //pinMode(D2,INPUT);
-  pinMode(D6,OUTPUT);
-  digitalWrite(D6,HIGH);
-  f1 = analogRead(A0) - 40;
-}
-
-void readFoto(){
-  readFoto1();
-  readFoto2();
-}
-
-
-void readFoto2(){
-  f2storico = f2;
-  pinMode(D6,INPUT);
-  //pinMode(D2,INPUT);
-  pinMode(D5,OUTPUT);
-  digitalWrite(D5,HIGH);
-  f2 = analogRead(A0) + 40;
-}
-
-int max(int array[],int len){
-  int max = 0;
-  for(int i=1;i<len;i++){
-    if(array[i] > array[i-1]){
-      max = i;
-    }
-  }
-  return max;
-}
-
-void startRun(){
-  analogWrite(D0,600);
-  analogWrite(D1,0);
-  analogWrite(D2,0);
-  analogWrite(D3,600);
-}
-
-void taratura(){
-  /*for(int i=0;i<8;i++){
-    analogWrite(D0,700);
-    analogWrite(D1,0);
-    analogWrite(D3,0);
-    analogWrite(D4,0);
-    readFoto1();
-    tarvalues[i] = f1;
-    delay(450);
-    stop();
-    delay(500);
-  }*/
-  stop();
-  /*delay(2000);
-  int index = max(tarvalues,8);
-  gira(index);
-  startRun();
-  Serial.print("[ ");
-  for(int i=0;i<8;i++){
-    Serial.print(tarvalues[i]);
-    Serial.print(",");
-  }
-  Serial.println( " ]");*/
+  pinMode(EnginePin1,OUTPUT);
+  pinMode(EnginePin2,OUTPUT);
+  pinMode(EnginePin3,OUTPUT);
+  pinMode(EnginePin4,OUTPUT);
+  analogWrite(EnginePin1,velocita);
+  analogWrite(EnginePin2,0);
+  analogWrite(EnginePin3,0);
+  analogWrite(EnginePin4,velocita);
 }
 
 void loop() {
@@ -259,4 +217,4 @@ void loop() {
     reconnect();
   }
   client.loop();
-  }
+ }
